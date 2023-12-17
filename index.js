84bf810b6c906e14c584082a3c3274c358ba072e
sabbir20201 <sabbirsarkar674@gmail.com>
2023-12-17 11:54:05 +0600

    modify some code

--- index.js
+++ index.js
@@ -25,12 +25,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const serviceCollection = client.db("surviceDb").collection("survices");
+
+    app.get("/service", async(req, res)=>{
+      const result = await serviceCollection.find().toArray();
+      res.send(result)
+      
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
